e6e10f0275f5749e5e0bf2ec2396e585910f5898
5h15h1r <shishir.agrahari14@gmail.com>
2024-10-27 22:05:30 +0530

    fix: component folder

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,21 +1,28 @@
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/";
+} from "@/components/ui/accordion";
 
 const members = () => {
   return (
-    <Accordion type="single" collapsible>
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+    <div>
+      <Navbar />
+      <div className="flex flex-col">
+      <div className="max-w-6xl">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Is it accessible?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+      </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -6,7 +6,7 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "../../components/ui/carousel";
+} from "./ui/carousel";
 
 interface CarouselImage {
   id: number;
